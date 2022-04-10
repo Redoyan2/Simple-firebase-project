13ca8a14b11aaaab72370053b71ed42c8a333af2
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-04-11 00:59:19 +0600

    signout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from './firebase.init';
+import { useState } from 'react';
 
 
 const auth = getAuth(app)
 
 function App() {
+  const [user, setUser] = useState({});
+
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn=()=>{
     signInWithPopup(auth, provider)
     .then(result=>{
-      console.log(result.user)
+      setUser(result.user);
+      console.log(result.user);
+      
     })
     .catch(error=>{
       console.error('error', error)
     })
     
   }
+
+  const handleGoogleSignOut=()=>{
+    signOut(auth)
+    .then(()=>{
+      setUser({});
+    })
+    .catch(error=>{
+      setUser({});
+      console.error('error', error);
+    })
+  }
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn} >Sign in by Google</button>
+      {
+        user.email ? <button onClick={handleGoogleSignOut}>Google Sign Out </button>
+         : 
+        <button onClick={handleGoogleSignIn} >Sign in by Google</button>
+      }
+
+      <h1>Name: {user.displayName}</h1>
+      <h2>Email:{user.email}</h2>
+      
+      <img src={user.photoURL} alt="" />
+
+      
+     
     </div>
   );
 }
