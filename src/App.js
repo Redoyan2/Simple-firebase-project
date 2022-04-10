e08963e9890a9c60d3ad9e9fff68e0bcf67edfe4
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-04-11 00:21:34 +0600

    google sign in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 
 import './App.css';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from './firebase.init';
+
+
+const auth = getAuth(app)
 
 function App() {
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSignIn=()=>{
+    signInWithPopup(auth, provider)
+    .then(result=>{
+      console.log(result.user)
+    })
+    .catch(error=>{
+      console.error('error', error)
+    })
+    
+  }
   return (
     <div className="App">
-      
+      <button onClick={handleGoogleSignIn} >Sign in by Google</button>
     </div>
   );
 }
